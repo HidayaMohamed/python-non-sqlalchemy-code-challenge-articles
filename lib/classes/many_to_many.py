19e064e58e7f2ec85e75ae5a55129205c9afeac3
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-11-17 04:06:17 +0300

    ADded validations for author class

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,24 @@
 class Article:
+    all= []
     def __init__(self, author, magazine, title):
+        # Validating the title 
         self.author = author
         self.magazine = magazine
         self.title = title
         
 class Author:
+    all = []
     def __init__(self, name):
+        # verify the author name propertites
+        if not isinstance(name, str):
+            return "Name must be a string"
+        if len(name) == 0:
+            return "Name must be longer than 0 characters."
         self.name = name
+        Author.all.append(self)
+    
+    def name(self):
+        return self._name
 
     def articles(self):
         pass
