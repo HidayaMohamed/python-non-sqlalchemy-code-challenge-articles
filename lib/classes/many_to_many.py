ccefc31cdabce2780c7521f8e9cd1e1d7b9853bb
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-11-17 04:25:01 +0300

    Added relationship methods

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,4 +1,5 @@
 class Article:
+    # keep track of every author created.
     all= []
     def __init__(self, author, magazine, title):
         # Validating the title 
@@ -11,26 +12,35 @@ class Author:
     def __init__(self, name):
         # verify the author name propertites
         if not isinstance(name, str):
-            return "Name must be a string"
+            raise TypeError("Name must be a string") 
         if len(name) == 0:
-            return "Name must be longer than 0 characters."
+            raise ValueError("Name must be longer than 0 characters.")
         self.name = name
         Author.all.append(self)
     
     def name(self):
+        # Author is now read only / cannot be changed.
         return self._name
 
     def articles(self):
-        pass
+        # Return all articles written by this author.
+        return [a for a in Article.all if a.author is self]
 
     def magazines(self):
-        pass
+        # Return a list of magazines this author contributed to. 
+        return list({a.magazine for a in self.articles()})
 
     def add_article(self, magazine, title):
-        pass
-
+        # Create a new article for this author.
+        return Article(self, magazine, title)
+    
     def topic_areas(self):
-        pass
+        # Return a unique list of categores of magazines this author wrote for.
+        mags= self.magazines()
+        if not mags :
+            return None
+        else :
+            return list({m.category for m in mags})
 
 class Magazine:
     def __init__(self, name, category):
